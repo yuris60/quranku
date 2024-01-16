13c612f112023565c3192815e5351c8980aebc8c
Yuris Alkhalifi <49089111+yuris60@users.noreply.github.com>
2024-01-16 22:37:28 +0700

    Edit File

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quranku/main.dart';
+import 'package:Quranku/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
