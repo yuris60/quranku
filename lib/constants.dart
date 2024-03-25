b5c442c0a7583360a802b34251ef05c8990748c8
Yuris Alkhalifi <49089111+yuris60@users.noreply.github.com>
2024-03-25 10:41:13 +0700

    Update Version

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 class Palette{
 
   //APP VERSION
-  static String app_version = "0.0.5";
-  static String whats_new = "Mempercepat proses baca data dengan menggunakan Cubit State Management.";
+  static String app_version = "0.0.6";
+  static String whats_new = "Menambahkan fitur doa sehari-hari, khatam quran, bookmark dan memperbarui tampilan.";
 
   // COLOR
-  static Color primary = Color(0xFF2a3881);
-  static Color secondary = Color(0xFFffe1a1);
+  static Color primary = Color(0xFF0061A8);
+  static Color primary_gradient = Color(0xff003271);
+  static Color secondary = Color(0xFFFF9002);
   static Color third = Color(0xFFd1a154);
   static Color dangers = Color(0xFFFF1053);
   static Color success = Color(0xFF107E7D);
   static Color warning = Color(0xFFE8C547);
   static Color info = Color(0xFF03A9F4);
+  static Color black = Color(0xFF333333);
   static Color greys = Colors.grey;
 }
